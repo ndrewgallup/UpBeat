eb95b248c3c3dd0ed7c800254ea11d2b1b782343
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-03-07 14:18:12 -0600

    embed review schema, rewrite show.ejs

--- models/album.js
+++ models/album.js
@@ -2,12 +2,31 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema ({
+  rTitle: {type: String, required: true},
+  rating: {
+    type: Number, 
+    min: 1, 
+    max: 10, 
+    required: true
+  },
+  favTracks: String,
+  content: {type: String, required: true},
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+}, {
+  timestamps: true
+})
+  
+
+
 const albumSchema = new Schema({
   title: String,
   artist: String, 
   year: String, 
   recordLabel: String,
+  reviews: [reviewSchema],
   owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+  
 }, {
   timestamps: true
 })
