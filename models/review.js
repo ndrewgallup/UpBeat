e80bb863edf58538c89dfd4f65e2bf8fa0ca7b43
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-03-07 11:40:27 -0600

    write review schema and create model review.js

--- models/review.js
+++ models/review.js
@@ -2,3 +2,16 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema({
+  rTitle: {type: String, required: true},
+  favTracks: String,
+  review: {type: String, required: true},
+}, {
+  timestamps: true
+})
+
+const Review = mongoose.model('Review', reviewSchema)
+
+export {
+  Review
+}
